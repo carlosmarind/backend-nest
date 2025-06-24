463096e302318f20ba75f9016e34bf39a09866de
Akash-Nkns <Niko.nkns@gmail.com>
2025-06-23 23:10:49 -0400

    Punto 2, se agrega la potencia

--- src/operaciones/operaciones.service.ts
+++ src/operaciones/operaciones.service.ts
@@ -11,6 +11,8 @@ export class OperacionesService {
       return this.#multiplicacion(a, b);
     } else if (operacion === 'division') {
       return this.#division(a, b);
+    } else if (operacion === 'potencia') {
+      return this.#potencia(a, b);
     } else {
       throw new Error(`Operación no válida: ${operacion}`);
     }
@@ -63,6 +65,18 @@ export class OperacionesService {
     }
  
     return a / b;
+  }
+
+  #potencia(base: number, exponente?: number) {
+    if (base === undefined || exponente === undefined) {
+      throw new Error('No se puede llamar con numeros indefinidos.');
+    }
+ 
+    if (typeof base !== 'number' || typeof exponente !== 'number') {
+      return NaN;
+    }
+ 
+    return Math.pow(base, exponente);
   }
  
  
